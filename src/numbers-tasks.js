d5829eb004f4c9ef0049b28f2ec9c19812649671
asharovatova <co.rin@mail.ru>
2024-12-03 17:13:04 +0300

    feat: implement getDistanceBetweenPoints

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -50,6 +50,8 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(/* value1, value2 */) {
+  // const sum = Number(value1) + Number(value2);
+  // return sum < Number.MAX_SAFE_INTEGER ? sum / 2 : 0;
   throw new Error('Not implemented');
 }
 
@@ -68,8 +70,9 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  return distance;
 }
 
 /**
