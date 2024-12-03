9e152c3796c2f738caab3bb7b2bfecaca7f4eef0
asharovatova <co.rin@mail.ru>
2024-12-03 21:02:30 +0300

    feat: implement getCountOfOddNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -625,8 +625,22 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  if (number === 0) return 0;
+
+  let count = 0;
+
+  if (number > 0) {
+    for (let i = 1; i <= number; i += 2) {
+      count += 1;
+    }
+  } else {
+    for (let i = -1; i >= number; i -= 2) {
+      count += 1;
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
